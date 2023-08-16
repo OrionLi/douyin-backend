ac60266114010c0a9a38454bc16765ea0116ef9e
OriTsuruHime <2711600752@qq.com>
2023-08-16 17:45:10 +0800

    video.go dao操作

--- video-center/main.go
+++ video-center/main.go
@@ -1,16 +1,15 @@
 package main
 
-import (
-	"douyin-backend/video-center/kitex_gen/videoCenter/videocenter"
-	"log"
-)
+import "douyin-backend/video-center/dao"
 
 func main() {
-	svr := videocenter.NewServer(new(VideoCenterImpl))
-
-	err := svr.Run()
-
-	if err != nil {
-		log.Println(err.Error())
-	}
+	//svr := videocenter.NewServer(new(VideoCenterImpl))
+	//
+	//err := svr.Run()
+	//
+	//if err != nil {
+	//	log.Println(err.Error())
+	//}
+	dao.Init()
+	dao.Migrate()
 }
