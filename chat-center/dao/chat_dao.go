8ae50e9ed9f02df3f240e00888a2ce1f7f75df5e
Raqptie <a2794109153@gmail.com>
2023-08-17 20:31:23 +0800

    feat:实现消息查询的Dao层

---
+++ chat-center/dao/chat_dao.go
@@ -0,0 +1,118 @@
+package dao
+
+import (
+	"douyin-backend/chat-center/generated/message"
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"log"
+	"strings"
+	"time"
+)
+
+// GetAllMessagesByToUserId 查询指定 toUserId 的消息列表
+//
+// 参数：toUserId
+//
+// 返回值：消息列表，错误
+func GetAllMessagesByToUserId(userId int) ([]message.Message, error) {
+
+	query := fmt.Sprintf(`
+	{
+	  "query": {
+	    "match": {
+	      "toUserId": %d
+	    }
+	  }
+	}`, userId)
+
+	res, err := ESClient.Search(
+		ESClient.Search.WithIndex("douyin_messages"), // 索引名
+		ESClient.Search.WithBody(strings.NewReader(query)),
+		ESClient.Search.WithPretty(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	messageList := GetMessageList(res)
+
+	return messageList, nil
+}
+
+// GetMessageByToUserId 查询指定 toUserId 的消息列表
+//
+// 参数：toUserId，preMsgTime
+//
+// 返回值：消息列表，错误
+func GetMessageByToUserId(time time.Time, userId int) ([]message.Message, error) {
+	query := fmt.Sprintf(`
+	{
+	  "query": {
+	    "bool": {
+	      "must": [
+	        {
+	          "match": {
+	            "toUserId": %d
+	          }
+	        },
+	        {
+	          "range": {
+	            "createTime": {
+	              "gte": "%s"
+	            }
+	          }
+	        }
+	      ]
+	    }
+	  }
+	}`, userId, time.Format("2006-01-02 15:04:05"))
+
+	res, err := ESClient.Search(
+		ESClient.Search.WithIndex("douyin_messages"), // 索引名
+		ESClient.Search.WithBody(strings.NewReader(query)),
+		ESClient.Search.WithPretty(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	messageList := GetMessageList(res)
+
+	return messageList, nil
+}
+
+// GetMessageList 将 Elasticsearch 返回的 Response 转换为 message.Message 列表
+func GetMessageList(res *esapi.Response) []message.Message {
+	var messageList []message.Message
+	var responseMap map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&responseMap); err != nil {
+		log.Fatalf("Error decoding JSON: %s", err)
+	}
+
+	hits := responseMap["hits"].(map[string]interface{})["hits"].([]interface{})
+
+	for _, hit := range hits {
+		source := hit.(map[string]interface{})["_source"].(map[string]interface{})
+		id := int(source["id"].(float64))
+		toUserId := int(source["toUserId"].(float64))
+		fromUserId := int(source["fromUserId"].(float64))
+		content := source["content"].(string)
+		createTime := source["createTime"].(string)
+
+		messageList = append(messageList, message.Message{
+			Id:         int64(id),
+			ToUserId:   int64(toUserId),
+			FromUserId: int64(fromUserId),
+			Content:    content,
+			CreateTime: createTime,
+		})
+	}
+	return messageList
+}
+
+// SendMessage 发送信息
+// TODO: 完成发送信息的数据库中添加操作
+func SendMessage() error {
+	return nil
+}
--- chat-center/service/chat_service.go
+++ chat-center/service/chat_service.go
@@ -1,33 +1,44 @@
 package service
 
-import "douyin-backend/chat-center/generated/message"
+import (
+	"douyin-backend/chat-center/dao"
+	"douyin-backend/chat-center/generated/message"
+	"log"
+	"strconv"
+)
 
 type ChatService interface {
-	GetAllHistoryMessage(toUserId string) (messageList []message.Message, err error)
-	GetMessage(toUserId string, preMsgTime string) (messageList []message.Message, err error)
-	SendMessage(toUserId string, fromUserId string, content string) (err error)
+	GetAllHistoryMessage(toUserId string) ([]message.Message, error)
+	GetMessage(toUserId string, preMsgTime int64) ([]message.Message, error)
+	SendMessage(toUserId string, fromUserId string, content string) error
 }
 
 type ChatServiceImpl struct{}
 
 // GetAllHistoryMessage 根据toUserId查询数据库中所有聊天记录
 // 当preMsgTime为0时，查询所有聊天记录
-func (c ChatServiceImpl) GetAllHistoryMessage(toUserId string) (messageList []message.Message, err error) {
-	//TODO implement me
-	panic("implement me")
+func (c ChatServiceImpl) GetAllHistoryMessage(toUserId string) ([]message.Message, error) {
+	// 查询相关记录
+	parseInt, err := strconv.ParseInt(toUserId, 10, 64)
+	if err != nil {
+		log.Fatalf("Parse error: %v", err)
+		return nil, err
+	}
+	messageList, err := dao.GetAllMessagesByToUserId(int(parseInt))
+	if err != nil {
+		return nil, err
+	}
+	return messageList, nil
 }
 
 // GetMessage 根据toUserId查询数据库中所有聊天记录
-func (c ChatServiceImpl) GetMessage(toUserId string, preMsgTime string) (messageList []message.Message, err error) {
+func (c ChatServiceImpl) GetMessage(toUserId string, preMsgTime int64) ([]message.Message, error) {
 	//TODO implement me
-	// 1. 根据toUserId查询数据库中所有聊天记录
-	// 2. 将聊天记录转换为message.Message
-	// 3. 返回messageList
 	panic("implement me")
 }
 
 // SendMessage 将消息存入数据库
-func (c ChatServiceImpl) SendMessage(toUserId string, fromUserId string, content string) (err error) {
+func (c ChatServiceImpl) SendMessage(toUserId string, fromUserId string, content string) error {
 	//TODO implement me
 	panic("implement me")
 }
