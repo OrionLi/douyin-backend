d04943d0ccf7e6e40b0da11963330f149347fcf0
Rapha-29 <2017151096@qq.com.com>
2023-08-19 15:57:00 +0800

    写了评论的部分代码

---
+++ server/video-center/service/commentService.go
@@ -0,0 +1,66 @@
+package service
+
+import (
+	"context"
+	"douyin-backend/server/video-center/dao"
+	"douyin-backend/server/video-center/model"
+	"time"
+)
+
+type CommentService struct {
+	ctx context.Context
+}
+
+func NewCommentService(context context.Context) *CommentService {
+	return &CommentService{ctx: context}
+}
+
+//todo 返回的东西还没有写好
+
+// SaveComment 发表评论
+func (s *CommentService) SaveComment(LoginUserId int64, videoId int64, content string) model.CommentApi {
+	// 发表评论
+	comment := dao.SaveComment(model.Comment{
+		Content:    content,
+		CreateDate: time.Now(),
+		UserId:     LoginUserId,
+		VideoId:    videoId,
+	})
+	//todo 通过用户ID查询用户然信息后封装
+	user := model.User{ID: comment.UserId}
+
+	CommentApi := model.ConvertToCommentApi(comment, user)
+	return CommentApi
+}
+
+// DeleteComment 删除评论
+func (s *CommentService) DeleteComment(LoginUserId int64, videoId int64, commentId int64) (bool, error) {
+	isComment, err := dao.IsUserComment(LoginUserId, commentId, videoId)
+	if err != nil {
+		return false, err
+	}
+	if isComment {
+		dao.DeleteComment(model.Comment{
+			VideoId: videoId,
+			ID:      commentId,
+		})
+		return true, nil
+	} else {
+		return false, nil
+	}
+}
+
+// ListComment 查看所有评论
+func (s *CommentService) ListComment(videoId int64) []model.CommentApi {
+	comments := dao.CommentList(videoId)
+	var commentApis []model.CommentApi
+	for _, comment := range comments {
+		//每个评论里面的userID comment.UserId
+		//todo 利用grpc查询user信息
+		user := model.User{ID: comment.UserId}
+
+		CommentApi := model.ConvertToCommentApi(comment, user)
+		commentApis = append(commentApis, CommentApi)
+	}
+	return commentApis
+}
