f4d1ad7d7797c2fd399358ad88f0bdf2443f8fba
Raqptie <a2794109153@gmail.com>
2023-08-21 20:31:55 +0800

    feat:新增video_interaction.proto，未生成pb文件

--- server/chat-center/test/rpc_test.go
+++ server/chat-center/test/rpc_test.go
@@ -2,61 +2,69 @@ package test
 
 import (
 	"context"
-	"fmt"
 	"github.com/OrionLi/douyin-backend/pkg/pb"
 	"google.golang.org/grpc"
-	"log"
+	"google.golang.org/grpc/credentials/insecure"
 	"testing"
 	"time"
 )
 
-// HACK 临时测试函数
+// TestGRPC 测试 gRPC 服务
 func TestGRPC(t *testing.T) {
+	t.Parallel() // 可选：允许并行运行测试
+
 	// 连接 gRPC 服务
-	conn, err := grpc.Dial("localhost:9422", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:9422", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
+		t.Fatalf("Failed to connect: %v", err)
 	}
-	defer conn.Close()
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			t.Fatalf("Failed to close gRPC connection: %v", err)
+		}
+	}(conn)
 
 	// 创建 gRPC 客户端
 	client := pb.NewDouyinMessageServiceClient(conn)
 
-	// 调用 gRPC 方法
-	getMessageRequest := &pb.DouyinMessageChatRequest{
-		SelfUserId: 1,
-		ToUserId:   123,
-		PreMsgTime: 0, // 设置合适的时间戳
-	}
+	t.Run("GetMessage", func(t *testing.T) {
+		// 调用 gRPC 方法
+		getMessageRequest := &pb.DouyinMessageChatRequest{
+			SelfUserId: 1,
+			ToUserId:   123,
+			PreMsgTime: 0, // 设置合适的时间戳
+		}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
 
-	getMessageResponse, err := client.GetMessage(ctx, getMessageRequest)
-	if err != nil {
-		log.Fatalf("Failed to call GetMessage: %v", err)
-	}
+		getMessageResponse, err := client.GetMessage(ctx, getMessageRequest)
+		if err != nil {
+			t.Fatalf("Failed to call GetMessage: %v", err)
+		}
 
-	// 打印响应
-	fmt.Printf("GetMessage Response: %+v\n", getMessageResponse)
+		// 打印响应
+		t.Logf("GetMessage Response: %+v", getMessageResponse)
+	})
 
-	fmt.Println("=====================================")
+	t.Run("SendMessage", func(t *testing.T) {
+		// 调用 gRPC 方法
+		sendMessageRequest := &pb.DouyinMessageActionRequest{
+			SelfUserId: 1,
+			ToUserId:   123,
+			Content:    "Hello World!",
+		}
 
-	// 调用 gRPC 方法
-	sendMessageRequest := &pb.DouyinMessageActionRequest{
-		SelfUserId: 1,
-		ToUserId:   123,
-		Content:    "Hello World!",
-	}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
 
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+		sendMessageResponse, err := client.SendMessage(ctx, sendMessageRequest)
+		if err != nil {
+			t.Fatalf("Failed to call SendMessage: %v", err)
+		}
 
-	sendMessageResponse, err := client.SendMessage(ctx, sendMessageRequest)
-	if err != nil {
-		log.Fatalf("Failed to call SendMessage: %v", err)
-	}
-
-	// 打印响应
-	fmt.Printf("SendMessage Response: %+v\n", sendMessageResponse)
+		// 打印响应
+		t.Logf("SendMessage Response: %+v", sendMessageResponse)
+	})
 }
