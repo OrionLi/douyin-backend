347b72e37ace812aab3e79c8f145b4dc76f17574
Raqptie <a2794109153@gmail.com>
2023-08-24 13:48:40 +0800

    feat:点赞Redis缓存的部分逻辑

--- server/video-center/cache/favoriteCache.go
+++ server/video-center/cache/favoriteCache.go
@@ -1,22 +1,49 @@
 package cache
 
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+	"video-center/pkg/util"
+)
+
 func ActionFavoriteCache(videoId int64, actionType int32) error {
-	// TODO 通过videoId创建键值对： key为favorite:videoId value为favorite_count
-	// TODO 当actionType为1时，value+1；当actionType为2时，value-1
-	// TODO 操作该值时，需要加锁，防止并发操作
-	// TODO 如果查询key为空，需要查询mysql中的值，然后设置到redis中
-	// TODO 如果查询key不为空，直接操作redis中的值
-	// TODO 操作完成后，需要解锁
-	// TODO 数据一致性策略： 异步更新mysql中的值
+	lockKey := fmt.Sprintf("lock:fav:vid:%d", videoId)
+	favoriteKey := fmt.Sprintf("favorite:%d", videoId)
+	lock, err := RedisLock(fmt.Sprintf(lockKey, videoId), 3*time.Second)
+	if err != nil || !lock {
+		return err
+	}
+	defer RedisUnlock(fmt.Sprintf(lockKey, videoId))
+	// 查询 Redis 中的值
+	favoriteCount, err := RedisClient.Get(context.Background(), favoriteKey).Int64()
+	if err != nil {
+		if err != redis.Nil {
+			return err
+		}
+		// TODO 查询mysql中的值
+	}
+	favoriteCount++
+	RedisClient.Set(context.Background(), favoriteKey, favoriteCount, 3*time.Minute)
+	// TODO 异步更新mysql中的值
 	return nil
 }
 
-func RedisLock() error {
-	// TODO 加锁
-	return nil
+// RedisLock redis分布式锁
+func RedisLock(lockKey string, lockTimeout time.Duration) (bool, error) {
+	lockAcquired, err := RedisClient.SetNX(context.Background(), lockKey, "lock-true", lockTimeout).Result()
+	if err != nil {
+		return false, err
+	}
+
+	return lockAcquired, nil
 }
 
-func RedisUnlock() error {
-	// TODO 解锁
-	return nil
+// RedisUnlock redis分布式锁解锁
+func RedisUnlock(lockKey string) {
+	_, err := RedisClient.Del(context.Background(), lockKey).Result()
+	if err != nil {
+		util.LogrusObj.Error("<Redis-FavoriteAction>, Unlock failed", err)
+	}
 }
