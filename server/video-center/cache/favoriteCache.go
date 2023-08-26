684e6d344e31ddf9bb3cca88e6db03da6cc25dd3
Raqptie <a2794109153@gmail.com>
2023-08-26 16:48:09 +0800

    fix：去掉点赞接口的分布式锁，通过原子性操作解决并发线程安全问题，点赞接口Redis部分压测已通过，MySQL无法承受大于每秒125个请求的高并发场景

--- server/video-center/cache/favoriteCache.go
+++ server/video-center/cache/favoriteCache.go
@@ -4,68 +4,32 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/go-redis/redis/v8"
 	"time"
-	"video-center/dao"
-	"video-center/pkg/util"
 )
 
 const FavoriteUpdateSetKey = "fav_update_set"
-const retryInterval = 5 * time.Millisecond
-const maxRetryInterval = 50 * time.Millisecond
 
 // ActionFavoriteCache 点赞缓存
+// 通过原子性操作解决并发问题
 func ActionFavoriteCache(videoId int64, actionType int32) error {
-	lockKey := fmt.Sprintf("lock:fav:vid:%d", videoId)
 	favoriteKey := fmt.Sprintf("favorite:%d", videoId)
-	lock, err := RedisLock(fmt.Sprintf(lockKey, videoId), 1*time.Second)
-	if err != nil {
-		return err
-	}
-	if !lock {
-		// FIXME 重试机制优化
-		var retryDelay = retryInterval
-		for {
-			lock, err := RedisLock(fmt.Sprintf(lockKey, videoId), 1*time.Second)
-			if err != nil {
-				return err
-			}
-			if lock {
-				break // 成功获取锁，退出重试循环
-			}
-			// 获取锁失败，等待一段时间后重试
-			time.Sleep(retryDelay)
-			if retryDelay < maxRetryInterval {
-				retryDelay += 10 * time.Millisecond
-			}
-		}
-	}
-	defer RedisUnlock(fmt.Sprintf(lockKey, videoId))
-	// 查询 Redis 中的值
-	favoriteCount, err := GetFavoriteCountCache(videoId)
-	if err != nil {
-		if err == redis.Nil {
-			count, err := dao.GetSingleVideoFavoriteCount(context.Background(), videoId)
-			if err != nil {
-				return err
-			}
-			favoriteCount = int64(count)
-		} else {
-			return err
-		}
-	}
 	switch actionType {
 	case 1:
-		favoriteCount++
+		// 使用 RedisClient 执行 INCR 命令
+		_, err := RedisClient.Incr(context.Background(), favoriteKey).Result()
+		if err != nil {
+			return err
+		}
 	case 2:
-		favoriteCount--
+		_, err := RedisClient.Decr(context.Background(), favoriteKey).Result()
+		if err != nil {
+			return err
+		}
 	default:
 		return errors.New("actionType error")
 	}
-	// 更新缓存
-	RedisClient.Set(context.Background(), favoriteKey, favoriteCount, 7*24*time.Hour)
 	// 更新集合
-	RedisClient.SAdd(context.Background(), FavoriteUpdateSetKey, videoId)
+	go RedisClient.SAdd(context.Background(), FavoriteUpdateSetKey, videoId)
 	return nil
 }
 
@@ -89,22 +53,4 @@ func DeleteVideoIdFromFavoriteUpdateSet(videoId int64) error {
 // GetMemberFromFavoriteUpdateSet 获取更新集合中的所有视频ID
 func GetMemberFromFavoriteUpdateSet() ([]string, error) {
 	return RedisClient.SMembers(context.Background(), FavoriteUpdateSetKey).Result()
-}
-
-// RedisLock redis分布式锁
-func RedisLock(lockKey string, lockTimeout time.Duration) (bool, error) {
-	lockAcquired, err := RedisClient.SetNX(context.Background(), lockKey, "lock-true", lockTimeout).Result()
-	if err != nil {
-		return false, err
-	}
-
-	return lockAcquired, nil
-}
-
-// RedisUnlock redis分布式锁解锁
-func RedisUnlock(lockKey string) {
-	_, err := RedisClient.Del(context.Background(), lockKey).Result()
-	if err != nil {
-		util.LogrusObj.Error("<Redis-FavoriteAction>, Unlock failed", err)
-	}
 }
