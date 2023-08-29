1602940b28ba3045fcee060d0e9bd45643db2961
Raqptie <a2794109153@gmail.com>
2023-08-29 18:57:34 +0800

    fix：删除chat模块部分文件

--- server/chat-center/pkg/common/msg.go
+++ server/chat-center/pkg/common/msg.go
@@ -1,11 +1,7 @@
 package common
 
 const (
-	SuccessMsg          = "success"
-	ErrorGetMsg         = "get error"
-	ErrorSendMsg        = "send error"
-	ParamErrorMsg       = "parameter error"
-	ActionTypeErrorMsg  = "action type not supported"
-	ContentNullErrorMsg = "content is null"
-	ForbiddenMsg        = "token forbidden"
+	SuccessMsg   = "success"
+	ErrorGetMsg  = "get error"
+	ErrorSendMsg = "send error"
 )
