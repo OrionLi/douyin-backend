1ced261aecba57e19b7ed36d834878a769da87c9
OrionLi <orionli@petalmail.com>
2023-08-31 15:30:09 +0800

    更新返回FriendUser内容

--- server/gateway-center/controller/relationController.go
+++ server/gateway-center/controller/relationController.go
@@ -5,6 +5,7 @@ import (
 	"gateway-center/pkg/e"
 	"gateway-center/response"
 	"gateway-center/util"
+	"github.com/OrionLi/douyin-backend/pkg/pb"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -88,6 +89,32 @@ func GetFriendList(ctx *gin.Context) {
 		})
 		return
 	}
+	// 更新分割线-----------------------
+	for _, friendUser := range resp.GetUserList() {
+		interActiveId := friendUser.GetUser().GetId()
+		messageResponse, _ := grpcClient.GetMessage(userId, interActiveId, 0)
+		list := messageResponse.GetMessageList()
+		message := list[len(list)-1]
+		friendUser = &pb.FriendUser{
+			User: &pb.User{
+				Id:              message.GetToUserId(),
+				Name:            "name",
+				FollowCount:     0,
+				FollowerCount:   0,
+				IsFollow:        true,
+				FavCount:        0,
+				WorkCount:       0,
+				TotalFavorited:  0,
+				BackgroundImage: "https://ts2.cn.mm.bing.net/th?id=OIP-C.HfZqICAPqMQslH0cMrIDFQHaKe&w=210&h=297&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2",
+				Signature:       "测试用户",
+				Avatar:          "https://ts2.cn.mm.bing.net/th?id=OIP-C.druUEHdZrBEuZPn2w80Y1QHaNK&w=187&h=333&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2",
+			},
+			Message: message.GetContent(),
+			MsgType: 0,
+		}
+	}
+
+	// 更新分割线-----------------------
 
 	ctx.JSON(http.StatusOK, response.GetFriendListResponse{
 		StatusCode: e.Success,
